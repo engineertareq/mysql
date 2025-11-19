773f907fe5e92b3571e7cb6844860c6c3b191812
engineertareq <engineertareqbd@gmail.com>
2025-11-19 13:14:55 +0600

    mysql d3

--- DDL/manage-from-comandline.php
+++ DDL/manage-from-comandline.php
@@ -14,3 +14,13 @@ USE command_db_t1;
  CREATE TABLE userss(id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(125) NOT NULL);
  <!-- Select Table -->
   SHOW COLUMNS FROM sdmsubscriptions;
+  <!-- Delete data from db table -->
+   delete from customers where customerID = 11;
+   delete from students where studentID = 11;
+
+   <!-- select from database table -->
+    select * from customers where customerID = 11;
+
+    <!-- Update Data -->
+      UPDATE customers SET customer_name = 'Karimul', city = 'Natore', contact_name = 'Islam' WHERE customers . customerID = 10;
+      
