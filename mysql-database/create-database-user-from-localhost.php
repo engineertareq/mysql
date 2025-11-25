26f39c7d630441a51bc90c4c6899fe91b33f2649
engineertareq <engineertareqbd@gmail.com>
2025-11-25 13:02:01 +0600

    adding db user

---
+++ mysql-database/create-database-user-from-localhost.php
@@ -0,0 +1,8 @@
+1. mysql -u root -p
+2. Enter password: (enter)
+3. show databases;
+4. create user 'jason'@'localhost' identified by 'abcd';
+5. exit
+6. mysql -u jason -p
+7. Enter password: ****
+8. show databases;
---
+++ mysql-database/provide-select-permisson-to-user.php
@@ -0,0 +1,7 @@
+mysql -u root
+GRANT SELECT ON pwad.* TO 'tareq'@'localhost';
+exit;
+mysql -u tareq -p
+passs:
+show databases;
+//you'll see permited databases
